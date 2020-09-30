98a31b8432fa8fedafc7b7118f018dbaa4e8f135
Illuminaxx <nexgen62@hotmail.fr>
2020-09-30 14:37:26 +0200

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="section is-medium">
+        <div class="container has-text-centered">
+            <h1 class="title">
+                <p>
+                    My name is Aurélien Hennuyer  (AKA Aurél), I’m a self taught software developer based in Arras, France. I build modern applications for the web!.
+                </p>
+            </h1>
+        </div>
+    </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Aurelien Hennuyer</h1>
+      <h2 class="subtitle">a Software Developer based in Arras, France.</h2>
+    </div>
+  </section>
+</template>
+
